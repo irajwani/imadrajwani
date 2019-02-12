5c637bb0eb453e2df5a4e5db8cb80dcedbf88764
Imad Rajwani <imadrajwani@gmail.com>
2019-02-12 19:56:03 +0500

    Animated profile pic, some more direction of how I want animations to unveil introductory details.

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,13 +3,12 @@ import "../../assets/stylesheets/Main.css";
 import "./Home.css";
 
 const headerOptions = ["Interests", "Projects", "Contact Me"];
-const introOne = "Hi, I'm Imad";
-const introTwo = "I like elegant design and modular code"
+const introText = ["Hi,\n I'm Imad", "I like elegant design & modular code"];
 
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  };
     }
     render() { 
         return ( 
@@ -27,11 +26,15 @@ class Home extends Component {
                 <div className="body fb col">
                     
                     <div className="fb row intro-container">
-                        <div className="fb intro-text-container">
-                            <div className="animated-intro-text-container">
-                                <p className="intro-text">{introOne}</p>
+                        <div className="fb col intro-profile-container">
+                            <div className="gradient">
+                                <img src={"https://avatars2.githubusercontent.com/u/25965881?s=460&v=4"} className="intro-image" alt="intro-image"/>
                             </div>
-                            
+                            {introText.map( (text) => 
+                            <div className="fb animated-intro-text-container">
+                              <h1 className="animated-intro-text">{text}</h1>
+                            </div>  
+                            )}
                         </div>
                         <div className="fb col intro-image-container">
 
